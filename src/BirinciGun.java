ab61b778e0a6693c79d7da9a5c6f76daf0ceebfe
husamettinszn <husamettinszn@gmail.com>
2020-11-17 21:40:12 +0100

    cozuldu mu

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -10,12 +10,8 @@ public class BirinciGun {
 		System.out.println("10:35 ikinci");
 		System.out.println("github.com");
 		System.out.println("gelmedi");
-<<<<<<< HEAD
+
 		System.out.println("ben buradan conflict beklemiyorum");
-=======
-		System.out.println("11:23 - github.com");
-		SYstem.out.println("Bakalim");
-		
->>>>>>> refs/remotes/origin/master
+
 	}
 }

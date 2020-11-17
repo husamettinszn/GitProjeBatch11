6a579feb7ed7fb69716b5637cda87b9c5e8ff871
husamettinszn <74509502+husamettinszn@users.noreply.github.com>
2020-11-17 20:14:23 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -4,5 +4,6 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("11:05 -  Birinci Gun");
 		System.out.println("Ikinci gun");
+		System.out.println("Ikinci Gun");
 	}
 }

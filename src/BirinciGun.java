fab68441be84333269ee09326fd7625815aeb581
husamettinszn <husamettinszn@gmail.com>
2020-11-17 21:33:49 +0100

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -7,6 +7,8 @@ public class BirinciGun {
 		System.out.println("Ikinci Gun");
 		System.out.println("yap");
 		System.out.println("10:35 ikinci");
-		System.out.println("11:05 -local");
+		System.out.println("github.com");
+		System.out.println("gelmedi");
+		System.out.println("ben buradan conflict beklemiyorum");
 	}
 }
